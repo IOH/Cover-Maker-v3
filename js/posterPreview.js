8e238acb6d3c8175bea38d3f664e023a188682d5
Hunsin <asky.hunsin@gmail.com>
2015-03-25 17:39:27 +0800

    discard useless files; add imgZoom function; poster-info style adjust

--- js/posterPreview.js
+++ js/posterPreview.js
@@ -1,5 +1,5 @@
 /*jslint white: true*/
-/*global $, jQuery, alert*/
+/*global $, alert*/
 
 $(document).ready(function() {
     
@@ -14,7 +14,7 @@ $(document).ready(function() {
         return size;
     }
     
-    // read image file & update src, resize to fit container, image draggable
+    // read image file & update src
     function readImg(file, name) {
         var reader = new FileReader(),
             img = new Image(),
@@ -42,10 +42,14 @@ $(document).ready(function() {
 
             // resize image to fit container & draggable
             if ((img.naturalWidth / img.naturalHeight) > (size.Width / size.Height)) {
-                $id.css({"width": "100%", "height": ""}).css({"top": 0.5 * (size.Height - $id.height()), "left": 0}).draggable();
+                $id.css({"width": "100%", "height": ""})
+                   .css({"top": 0.5 * (size.Height - $id.height()), "left": 0})
+                   .draggable().imgZoom();
             }
             else {
-                $id.css({"width": "", "height": "100%"}).css({"top": 0, "left": 0.5 * (size.Width - $id.width())}).draggable();
+                $id.css({"width": "", "height": "100%"})
+                   .css({"top": 0, "left": 0.5 * (size.Width - $id.width())})
+                   .draggable().imgZoom();
             }
 
             //alert if image size is too small
@@ -62,18 +66,49 @@ $(document).ready(function() {
         resetImg(name);
     });
     
-    // info box resizable, draggable & align with profile picture
-    $("#poster-info").resizable({grid: [16, 16]})
+    // info box resizable, draggable & align with profile image
+    $("#poster-info").resizable({grid: [16, 24]})
                      .draggable({
                          containment: "#poster-background",
                          grid: [16, 16]
                      });
     
-    // change position of profile picture
+    // change position of profile image
     $("#click-left").click(function() {
         $("#poster-profile, #poster-location").css("left", "32px");
     });
     $("#click-right").click(function() {
         $("#poster-profile, #poster-location").css("left", "768px");
     });
+    
+    // under is texting code for zooming image
+    $.fn.imgZoom = function() {
+        var img = this.get(0);
+        
+        function MouseWheel(e) {
+            var delta = (e.wheelDelta > 0)? 0.05 : -0.05,
+                deltaW = delta * this.width,
+                deltaH = delta * this.height,
+                mouseX = e.offsetX,
+                mouseY = e.offsetY,
+                imgX = $(this).offset().left,
+                imgY = $(this).offset().top;
+
+            window.event.preventDefault();
+            
+            console.log(this.height / this.width);
+            
+            $(this).width(this.width + deltaW);
+            $(this).height(this.height + deltaH);
+            $(this).offset({
+                top: imgY - deltaH * mouseY / this.height,
+                left: imgX - deltaW * mouseX / this.width
+            });
+            
+            return false;
+        }
+        
+        img.addEventListener("mousewheel", MouseWheel, false); // IE9 up, Chrome, Safari, Opera
+        img.addEventListener("DOMMouseScroll", MouseWheel, false); // Firefox
+    };
 });
