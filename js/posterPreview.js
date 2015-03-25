5f0e41ffcd835acd9202d25239783c1f817a92aa
Hunsin <asky.hunsin@gmail.com>
2015-03-25 12:06:07 +0800

    rewrite code; image place to center when upload

--- js/posterPreview.js
+++ js/posterPreview.js
@@ -1,19 +1,30 @@
+/*jslint white: true*/
+/*global $, jQuery, alert*/
+
 $(document).ready(function() {
     
-    // read image file and update src
-    function readImg(file, $id) {
-        var reader = new FileReader();
-        var img = new Image();
+    // return the container size
+    function containerSize(name) {
+        var $id = $("#poster-" + name),
+            size = {
+                Width: $id.width(),
+                Height: $id.height()
+            };
+            
+        return size;
+    }
+    
+    // read image file & update src, resize to fit container, image draggable
+    function readImg(file, name) {
+        var reader = new FileReader(),
+            img = new Image(),
+            $id = $("#img-" + name);
         
         reader.readAsDataURL(file);
-        reader.onload = function(_file) {
-            img.src = _file.target.result;
+        reader.onload = function(file) {
+            img.src = file.target.result;
             img.onload = function() {
                 $id.attr("src", this.src);
-                
-                // set image to original dimension
-                $id.width(this.naturalWidth);
-                $id.height(this.naturalHeight);
             };
             img.onerror = function() {
                 alert("僅接受圖片");
@@ -21,26 +32,37 @@ $(document).ready(function() {
         };
     }
     
-    // renew image & makes it draggable while uploaded
-    $("#upload-background").change(function(e) {
-        var $id = $("#img-background");
-        $.when(readImg(this.files[0], $id)).then(function() {
-            
-            // position to origin
-            $id.css({"left": 0, "top": 0})
-               .draggable();
-        });
+    // resize image to fit container, image draggable
+    function resetImg(name) {
+        var $id = $("#img-" + name),
+            img = $id.get(0),
+            size = containerSize(name);
+
+        img.onload = function() {
+
+            // resize image to fit container & draggable
+            if ((img.naturalWidth / img.naturalHeight) > (size.Width / size.Height)) {
+                $id.css({"width": "100%", "height": ""}).css({"top": 0.5 * (size.Height - $id.height()), "left": 0}).draggable();
+            }
+            else {
+                $id.css({"width": "", "height": "100%"}).css({"top": 0, "left": 0.5 * (size.Width - $id.width())}).draggable();
+            }
+
+            //alert if image size is too small
+            if (img.naturalWidth < (size.Width * 2) || img.naturalHeight < (size.Height * 2)) {
+                alert("建議影像尺寸" + (size.Width * 2) + "x" + (size.Height * 2) + "以上，圖片可能太小囉！");
+            }
+        };
+    }
+
+    // update image & make it draggable
+    $(".upload-img").change(function(e) {
+        var name = $(this).attr("name");
+        readImg(this.files[0], name);
+        resetImg(name);
     });
     
-    $("#upload-profile").change(function(e) {
-        var $id = $("#img-profile");
-        $.when(readImg(this.files[0], $id)).then(function() {
-            $id.css({"left": 0, "top": 0})
-               .draggable();
-        });
-    });
-    
-    // make sure info box align with profile picture & text
+    // info box resizable, draggable & align with profile picture
     $("#poster-info").resizable({grid: [16, 16]})
                      .draggable({
                          containment: "#poster-background",
